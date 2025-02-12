8caee50de1067f71f5378b5197e790d4f12ca689
Kelvin Kipkoech Chepkwony <chepkwonyke1@GMAIL.COM>
2025-02-12 21:58:23 +0300

    Chore: simple api

--- 01-node-farm/index.js
+++ 01-node-farm/index.js
@@ -36,6 +36,11 @@ const url = require("url");
 ////////////////////////////////
 // SERVER
 
+const data = fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8");
+
+// Convert the JSON string to a JavaScript object
+const dataObj = JSON.parse(data);
+
 // Create a simple HTTP server
 const server = http.createServer((req, res) => {
   console.log(req.url);
@@ -44,6 +49,12 @@ const server = http.createServer((req, res) => {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    const productData = JSON.parse(data);
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-Type": "text/html",
