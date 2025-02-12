c8b2ec2c1610fe74c99f966d76da0a8766a69eaf
Kelvin Kipkoech Chepkwony <chepkwonyke1@GMAIL.COM>
2025-02-12 21:47:18 +0300

    Chore: mastered routing

--- 01-node-farm/index.js
+++ 01-node-farm/index.js
@@ -4,6 +4,7 @@ console.log(hello);
 // Import the fs module
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 ////////////////////////////////
 // FILES
@@ -37,8 +38,18 @@ const http = require("http");
 
 // Create a simple HTTP server
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end(`Hello from the server!`);
+  console.log(req.url);
+  const pathName = req.url;
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+    }); // Status code 404: Not Found
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
