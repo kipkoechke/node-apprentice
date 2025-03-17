1d67f07e3bec245bad082c5e9ebfa827e0439848
kipkoechke <chepkwonyke1@gmail.com>
2025-03-17 16:00:45 +0300

    Chore: error handling

---
+++ 04-natours/controllers/errorController.js
@@ -0,0 +1,60 @@
+const AppError = require("../utils/appError");
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value} already exists. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = errors.join(". ");
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    // Log error for debugging purposes
+    console.error("ERROR:", err);
+
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong, please try again later."
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  if (process.env === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env === "production") {
+    let error = { ...err };
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+    sendErrorProd(error, res);
+  }
+};
--- 04-natours/server.js
+++ 04-natours/server.js
@@ -1,6 +1,12 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
+process.on("uncaughtException", err => {
+  console.error("UNCAUGHT EXCEPTION! Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
@@ -12,4 +18,14 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`App running on port ${port}...`));
+const server = app.listen(port, () =>
+  console.log(`App running on port ${port}...`)
+);
+
+process.on("unhandledRejection", err => {
+  console.error(err.name, err.message);
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
+});
