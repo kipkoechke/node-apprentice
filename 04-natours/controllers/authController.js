7194d6a479869b85bbfd329091d43d4092a9e383
kipkoechke <chepkwonyke1@gmail.com>
2025-03-18 18:58:54 +0300

    Chore: protect routes fron unautherized access

--- 04-natours/controllers/authController.js
+++ 04-natours/controllers/authController.js
@@ -4,6 +4,7 @@ const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const AppError = require("./../utils/appError");
 
+// Sign token for authenticated user
 const signToken = id => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN
@@ -30,35 +31,60 @@ exports.signup = catchAsync(async (req, res) => {
 
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
+  //1) Check if email and password exist
   if (!email || !password) {
     return next(new AppError("Please provide email and password", 400));
   }
+  //2) Check if user exists and password is correct
   const user = await User.findOne({ email }).select("+password");
 
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError("Incorrect email or password", 401));
   }
-
+  //3) If everything is ok, send token to client
   const token = signToken(user._id);
   res.json({ status: "success", token });
 });
 
-exports.protect = async (req, res, next) => {
+exports.protect = catchAsync(async (req, res, next) => {
   let token;
+  // 1) Check if token is in the authorization header
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     token = req.headers.authorization.split(" ")[1];
+
+    if (!token) {
+      return next(new AppError("You are not logged in. Please log in.", 401));
+    }
+
+    // 2) Verification token
+    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+
+    // 3) Check if user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next(
+        new AppError(
+          "The user belonging to this token does no longer exist",
+          401
+        )
+      );
+    }
+
+    // 4) Check if user changed password after token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next(
+        new AppError(
+          "User has recently changed their password. Please log in again",
+          401
+        )
+      );
+    }
+
+    // Grant access to protected route
+    req.user = currentUser;
+    next();
   }
-  if (!token) {
-    return next(new AppError("You are not logged in. Please log in.", 401));
-  }
-  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
-  const currentUser = await User.findById(decoded.id);
-  if (!currentUser) {
-    return next(new AppError("User no longer exists", 401));
-  }
-  req.user = currentUser;
-  next();
-};
+});
--- 04-natours/models/userModel.js
+++ 04-natours/models/userModel.js
@@ -33,7 +33,8 @@ const userSchema = new mongoose.Schema({
       },
       message: "Passwords do not match"
     }
-  }
+  },
+  passwordChangedAt: Date
   //   role: {
   //     type: String,
   //     enum: ["user", "admin"],
@@ -43,19 +44,37 @@ const userSchema = new mongoose.Schema({
   //   tokenExpiration: Date
 });
 
+// MIDDLEWARE: runs before.save() and.create() but only if password was modified
 userSchema.pre("save", async function(next) {
+  // Check if password was modified before hashing it
   if (!this.isModified("password")) return next();
 
+  // Hash the password before saving it to the database
   this.password = await bcrypt.hash(this.password, 12);
   this.passwordConfirm = undefined;
   next();
 });
 
+// INSTANCE METHODS: check if candidate password matches the stored hashed password
 userSchema.methods.correctPassword = async function(
   candidatePassword,
   userPassword
 ) {
+  // Compare the hashed candidate password with the stored hashed password
   return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+//INSTANCE METHOD: check whether the password was changed
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+  if (this.passwordChangedAt) {
+    const changedTimestamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10
+    );
+    // Check if JWTTimestamp is older than the passwordChangedAt timestamp (i.e. password was changed)
+    return JWTTimestamp < changedTimestamp;
+  }
+  return false;
 };
 
 const User = mongoose.model("User", userSchema);
