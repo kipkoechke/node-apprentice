ac93f1da951f7918cba050d4211df7993bf7c4ee
kipkoechke <chepkwonyke1@gmail.com>
2025-03-18 19:12:27 +0300

    Chore: user roles and permissions

--- 04-natours/models/userModel.js
+++ 04-natours/models/userModel.js
@@ -18,6 +18,11 @@ const userSchema = new mongoose.Schema({
     validate: [validator.isEmail, "Please provide a valid email address"]
   },
   photo: String,
+  role: {
+    type: String,
+    enum: ["user", "guide", "lead-guide", "admin"],
+    default: "user"
+  },
   password: {
     type: String,
     required: [true, "Please provide a password"],
@@ -35,11 +40,6 @@ const userSchema = new mongoose.Schema({
     }
   },
   passwordChangedAt: Date
-  //   role: {
-  //     type: String,
-  //     enum: ["user", "admin"],
-  //     default: "user"
-  //   },
   //   token: String,
   //   tokenExpiration: Date
 });
