470d2c564891e1f2c1d823a054163f337f47ba52
kipkoechke <chepkwonyke1@gmail.com>
2025-03-19 13:34:42 +0300

    Chore: rate limiting and set security http headers

--- 04-natours/app.js
+++ 04-natours/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 
@@ -8,14 +10,34 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+//-- 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-app.use(express.json());
 
+// Limit requests from same IP address
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour."
+});
+app.use("/api", limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// 2) ROUTES
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
+// 3) ERROR HANDLING
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
 });
